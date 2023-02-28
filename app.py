104459895276a9f1a838248c36aa03b7e99460c6
Kahsolt <kahsolt@qq.com>
2023-02-28 12:53:33 +0800

    add webui tester client

--- app.py
+++ app.py
@@ -3,51 +3,95 @@
 # Create Time: 2023/02/28 
 
 import os
+import yaml
 from pathlib import Path
 from traceback import format_exc
 
-from flask import Flask, request
+import numpy as np
+from flask import Flask, request, jsonify
 
+from modules.util import seed_everything
 import run as RT
 
 BASE_PATH = Path(__file__).parent
 LOG_PATH = BASE_PATH / 'log'
 
 app = Flask(__name__)
-cur_expname = None
+cur_job: str = None
 
 
-@app.route('/list')
+@app.route('/', methods=['GET'])
 def index():
-  return [exp.name for exp in LOG_PATH.iterdir() if exp.is_dir()]
+  html = '''
+  <h3> Available API: </h3>
+  <ul>
+    <li><a href="/job/list">        GET  /job/list  </a></li>
+    <li><a href="/job/XGBoost_clf"> GET  /job/:name </a></li>
+    <li><a href="/infer ">          POST /infer     </a></li>
+    <li><a href="/debug">           GET  /debug     </a></li>
+  </ul>
+'''
+  return html
 
 
-@app.route('/predict')
-def hana_tap():
-  req = request.get_json()
-  print(f'>> req: {req}')
+@app.route('/job/list', methods=['GET'])
+def job_list():
+  try:
+    return jsonify([job.name for job in LOG_PATH.iterdir() if job.is_dir()])
+  except:
+    return jsonify({'error': format_exc()})
+
+
+@app.route('/job/<name>', methods=['GET'])
+def job_(name):
+  try:
+    with open(LOG_PATH / name / 'job.yaml', 'r', encoding='utf-8') as fh:
+      config = yaml.safe_load(fh)
+    return jsonify(config)
+  except:
+    return jsonify({'error': format_exc()})
+
+
+@app.route('/infer', methods=['POST'])
+def infer():
+  global cur_job
 
   try:
-    expname = req['expname']
-    x = req['x']
+    req = request.get_json()
+    job = req['job']
+    x = np.asarray(req['x'])
+    print(f'>> job: {job}')
+    print(f'>> x.shape: {x.shape}')
 
-    if expname != cur_expname:
-      RT.env = { }
-      RT.job = RT.load_job(LOG_PATH / expname / 'job.yaml')
+    if job != cur_job:
+      # init job
+      RT.job = RT.load_job(LOG_PATH / job / 'job.yaml')
+      RT.env.clear()
+      RT.env['log_dp'] = LOG_PATH / job
+      seed_everything(RT.job_get('misc/seed'))
+
+      # load job states
+      @RT.require_model
+      @RT.require_data
+      def load_model_and_data():
+        RT.env['model'] = RT.env['manager'].load(RT.env['model'], RT.env['log_dp'])
+      load_model_and_data()
+
+      # get context
       manager = RT.env['manager']
       model = RT.env['model']
-      cur_expname = expname
+      cur_job = job
 
     y = manager.infer(model, x)
-    return {'y': y.tolist()}
+    return jsonify({'y': y.tolist()})
   except:
-    return {'error': format_exc()}
+    return jsonify({'error': format_exc()})
 
 
-@app.route('/debug')
+@app.route('/debug', methods=['GET'])
 def debug():
   s = ''
-  s += f'<p>os.getcwd(): {os.getcwd()}</p>'
+  s += f'<p>pwd: {os.getcwd()}</p>'
   s += f'<p>BASE_PATH: {BASE_PATH}</p>'
   s += f'<p>LOG_PATH: {LOG_PATH}</p>'
   return s
