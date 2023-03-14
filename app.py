e716b13ee001eecd12b2125897ed04ae40f1d6ee
Kahsolt <kahsolt@qq.com>
2023-03-14 17:01:03 +0800

    add index.html, /metric

--- app.py
+++ app.py
@@ -8,30 +8,22 @@ from pathlib import Path
 from traceback import format_exc
 
 import numpy as np
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 
 from modules.util import seed_everything
 import run as RT
 
 BASE_PATH = Path(__file__).parent
+DATA_PATH = BASE_PATH / 'data'
 LOG_PATH = BASE_PATH / 'log'
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='.')
 cur_job: str = None
 
 
 @app.route('/', methods=['GET'])
 def index():
-  html = '''
-  <h3> Available API: </h3>
-  <ul>
-    <li><a href="/job/list">        GET  /job/list  </a></li>
-    <li><a href="/job/XGBoost_clf"> GET  /job/:name </a></li>
-    <li><a href="/infer ">          POST /infer     </a></li>
-    <li><a href="/debug">           GET  /debug     </a></li>
-  </ul>
-'''
-  return html
+  return render_template('index.html')
 
 
 @app.route('/job/list', methods=['GET'])
@@ -52,6 +44,38 @@ def job_(name):
     return jsonify({'error': format_exc()})
 
 
+@app.route('/metric/list', methods=['GET'])
+def metric_list():
+  try:
+    with open(LOG_PATH / 'metric-ranklist.txt', 'r', encoding='utf-8') as fh:
+      data = fh.read().strip()
+    ret = { }
+    for sec in data.split('\n\n'):
+      lines = sec.split('\n')
+      title = lines[0][:-1]     # remove ':'
+      ret[title] = { }
+      for line in lines[1:]:
+        name, score = line.split(':')
+        ret[title][name] = float(score)
+    return jsonify(ret)
+  except:
+    return jsonify({'error': format_exc()})
+
+
+@app.route('/metric/<name>', methods=['GET'])
+def metric_(name):
+  try:
+    with open(LOG_PATH / name / 'metric.txt', 'r', encoding='utf-8') as fh:
+      data = fh.read().strip()
+    ret = {}
+    for line in data.split('\n'):
+      name, score = line.split(':')
+      ret[name] = float(score)
+    return jsonify(ret)
+  except:
+    return jsonify({'error': format_exc()})
+
+
 @app.route('/infer', methods=['POST'])
 def infer():
   global cur_job
@@ -59,7 +83,7 @@ def infer():
   try:
     req = request.get_json()
     job = req['job']
-    x = np.asarray(req['x'])
+    x = np.asarray(req['x'], dtype=np.float32)
     print(f'>> job: {job}')
     print(f'>> x.shape: {x.shape}')
 
