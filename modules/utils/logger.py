ebb596c9f16628e388b427b13e1acd560aae8b11
Kahsolt <kahsolt@qq.com>
2024-03-19 11:38:45 +0800

    fix duplicated logging

--- modules/utils/logger.py
+++ modules/utils/logger.py
@@ -6,22 +6,31 @@ import sys
 from pathlib import Path
 import logging
 from logging import Logger
+from typing import Dict
 
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
+loggers: Dict[str, Logger] = {}
+
 
 def get_logger(name, log_dp=Path('.')) -> Logger:
-  logger = logging.getLogger(name)
-  logger.setLevel(logging.WARN)
-  formatter = logging.Formatter("%(asctime)s\t%(name)s\t%(levelname)s\t%(message)s")
-  h = logging.FileHandler(log_dp / 'job.log')
-  h.setLevel(logging.DEBUG)
-  h.setFormatter(formatter)
-  logger.addHandler(h)
-  return logger
+  if name not in loggers:
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.WARN)
+    formatter = logging.Formatter("%(asctime)s\t%(name)s\t%(levelname)s\t%(message)s")
+    h = logging.FileHandler(log_dp / 'job.log')
+    h.setLevel(logging.DEBUG)
+    h.setFormatter(formatter)
+    logger.addHandler(h)
+    loggers[name] = logger
+  return loggers[name]
 
 
 def close_logger(logger:Logger):
+  for name, log in loggers.items():
+    if log is logger:
+      del loggers[name]
+      break
   for handler in logger.handlers:
     if isinstance(handler, logging.FileHandler):
       handler.flush()
